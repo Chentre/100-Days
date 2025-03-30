4f2f039b216a3d393d802a6912ba3ba8e3d00be3
Chentre <2117423882@qq.com>
2025-03-30 16:53:03 +0800

    test

--- pytorch-tutorial/tutorials/01-basics/pytorch_basics/main.py
+++ pytorch-tutorial/tutorials/01-basics/pytorch_basics/main.py
@@ -127,37 +127,38 @@ images, labels = data_iter.__next__()
 # Actual usage of the data loader is as below.
 for images, labels in train_loader:
     # Training code should be written here.
+
     pass
 
 
-# ================================================================== #
-#                5. Input pipeline for custom dataset   自定义数据集的输入管道              #
-# ================================================================== #
+# # ================================================================== #
+# #                5. Input pipeline for custom dataset   自定义数据集的输入管道              #
+# # ================================================================== #
 
-# You should build your custom dataset as below.
-class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        # TODO
-        # 1. Initialize file paths or a list of file names.
-        pass
+# # You should build your custom dataset as below.
+# class CustomDataset(torch.utils.data.Dataset):
+#     def __init__(self):
+#         # TODO
+#         # 1. Initialize file paths or a list of file names.
+#         pass
 
-    def __getitem__(self, index):
-        # TODO
-        # 1. Read one data from file (e.g. using numpy.fromfile, PIL.Image.open).
-        # 2. Preprocess the data (e.g. torchvision.Transform).
-        # 3. Return a data pair (e.g. image and label).
-        pass
+#     def __getitem__(self, index):
+#         # TODO
+#         # 1. Read one data from file (e.g. using numpy.fromfile, PIL.Image.open).
+#         # 2. Preprocess the data (e.g. torchvision.Transform).
+#         # 3. Return a data pair (e.g. image and label).
+#         pass
 
-    def __len__(self):
-        # You should change 0 to the total size of your dataset.
-        return 0
+#     def __len__(self):
+#         # You should change 0 to the total size of your dataset.
+#         return 0
 
 
-# You can then use the prebuilt data loader.
-custom_dataset = CustomDataset()
-train_loader = torch.utils.data.DataLoader(dataset=custom_dataset,
-                                           batch_size=64,
-                                           shuffle=True)
+# # You can then use the prebuilt data loader.
+# custom_dataset = CustomDataset()
+# train_loader = torch.utils.data.DataLoader(dataset=custom_dataset,
+#                                            batch_size=64,
+#                                            shuffle=True)
 
 
 # ================================================================== #
